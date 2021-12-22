4147fedefec138a4c0950568621dad69c1db2d72
uukkii <i.am.newbie@hotmail.com>
2021-12-22 17:47:52 +0300

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,56 @@
+import com.fasterxml.jackson.annotation.JacksonAnnotation;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Post {
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public Post(@JsonProperty("id") String id,
+                @JsonProperty("text") String text,
+                @JsonProperty("type") String type,
+                @JsonProperty("user") String user,
+                @JsonProperty("upvotes") int upvotes) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+
+    public String toString() {
+        return
+                "{\n" +
+                        "id: " + id + "\n" +
+                        "text: " + text + "\n" +
+                        "type: " + type + "\n" +
+                        "user: " + user + "\n" +
+                        "upvotes: " + upvotes + "\n" +
+                        "}\n";
+    }
+}
